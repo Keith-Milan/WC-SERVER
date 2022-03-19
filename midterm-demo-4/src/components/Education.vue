85797ffeff1da9642e793ff729b3b6b4f482d751
Keith-Milan <100083905+Keith-Milan@users.noreply.github.com>
2022-03-20 03:02:56 +0800

    Add files via upload

---
+++ midterm-demo-4/src/components/Education.vue
@@ -0,0 +1,43 @@
+<!---Name: Milan, Janus Keith T.
+     Code Name: education.vue 
+     Section:WD-202-->
+
+<template>
+  <div id="education">
+
+    <h2>{{JNH}}<br> HOLY FAMILY ACADEMY <br> HOLY FAMILY ACADEMY. <br>Location: Angeles City </h2>
+  
+  
+
+    <h2>{{SNH}}<br> HOLY FAMILY ACADEMY from 2018 - 2020/11-12. <br>Location: Angeles City </h2>
+   
+
+    <h2>{{CLL}}<br> HOLY ANGEL UNIVERSITY from 2020 - 2022 <br>Location: Angeles City</h2>
+    
+
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'education',
+  data () {
+    return {
+        JNH:'JUNIOR HIGHSCHOOL:',
+
+        SNH:'SENIOR HIGHSCHOOL:',
+
+        CLL:'COLLEGE:'
+      
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ midterm-demo-4/src/components/Profile.vue
@@ -0,0 +1,124 @@
+<!---Name: Milan, Janus Keith T.
+     Code Name: profile.vue
+     Section:WD-202-->
+
+<template>
+  <div id="app">
+    
+    <form> <br/>
+    <h1>{{name}} Janus Keith T. Milan </h1>
+    
+
+    <h2>{{course}} BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY</h2>
+
+    <h2>{{spec}} WEB DEVELOPER</h2>
+
+    <h2>{{bdate}} 6/16/2001</h2>
+    
+
+    <h2>{{age}} 20</h2>
+ 
+
+    <h2>{{gender}} Male</h2>
+  
+
+    <h2>{{goal}}<br> My goal in this school year is to gain more knowledge about this course.<br><br>For me to have a nice and stable job.</h2>
+    
+      
+   
+  </form>
+  
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+
+  data () {
+    return {
+      name:'NAME:',
+
+      course:'COURSE:',
+
+      spec:'SPECIALTY:',
+
+      bdate:'DATE OF BIRTH:',
+
+      age:'AGE:',
+
+      gender:'GENDER:',
+
+      goal:'GOAL:',
+    
+      
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Consolas', 'Courier New', monospace;
+  
+  color: burlywood;
+
+  float: left;
+
+  margin-left: 3%;
+
+  margin-top: 2%;
+ 
+  
+  
+  
+
+}
+
+
+body {
+  background-color: olivedrab;
+}
+
+form{
+  background-color:#d415a4;
+
+  height: 50em;
+
+  width: 50em;
+}
+
+h1{
+  font-size: 1em;
+
+  margin-left: 10%;
+
+  margin-top: 5%;
+
+  border: 	#ADD8E6;
+
+  border-color: 	#ADD8E6;
+}
+h2{
+  font-size: 1em;
+
+  margin-top: 3%;
+
+  margin-left: 10%;
+}
+
+p{
+  margin-top: 2%;
+
+  font-size: .9em;
+
+  margin-left: 11%;
+}
+
+
+
+</style>
